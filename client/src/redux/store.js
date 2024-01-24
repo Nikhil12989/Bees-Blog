defc57f4f9c15b37768f359666b780e7c4e4ca85
Nikhil Salwade <nikhilsalwade123@gmail.com>
2024-01-24 20:54:02 +0530

    add redux persist in bog

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-import  useReducer  from  './user/userSlice.js'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const rootReducer = combineReducers({
+  user: userReducer, // Corrected the import and usage of userReducer
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // Changed the variable name to avoid conflict
 
 export const store = configureStore({
-  reducer: {
-    user: useReducer,
-    
-  },
-})
+  reducer: persistedReducer, // Use persistedReducer here
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
