e25d54e70d7f8c45ffef49f735969645d6dc434a
Nikhil Salwade <nikhilsalwade123@gmail.com>
2024-01-25 20:08:01 +0530

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { BrowserRouter , Route , Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Header from './components/Header';
-import FooterCom from './components/Footer';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
-    <BrowserRouter >
-    <Header/>
+    <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route  path='/about' element={<About/>}/>
-        <Route path='/sign-in' element={<Signin/>}/>
-        <Route path='/sign-up' element={<Signup/>} />
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/projects' element={<Projects/>}/> 
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <FooterCom/>
+      <FooterCom />
     </BrowserRouter>
-  )
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector , useDispatch } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice.js";
 
 export default function Header() {
   const path = useLocation().pathname;
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const { currentUsers } = useSelector((state) => state.user);
-
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -34,8 +34,14 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2 ">
-        <Button className="w-12 h-10  sm:inline" color="gray" pill onClick={() =>dispatch(toggleTheme())}>
-          <FaMoon />
+        <Button
+          className="w-12 h-10  sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun/> : <FaMoon/>}
+        
         </Button>
         {currentUsers ? (
           <Dropdown
@@ -49,14 +55,10 @@ export default function Header() {
                 {currentUsers.email}
               </span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
-            <Dropdown.Item>
-              Profile
-            </Dropdown.Item>
-            <hr />
-            <Dropdown.Item>
-              Sign Out
-            </Dropdown.Item>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+              <hr />
+              <Dropdown.Item>Sign Out</Dropdown.Item>
             </Link>
           </Dropdown>
         ) : (
