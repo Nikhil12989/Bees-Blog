dac725dffe6a7e50f5181e6cbfa3a3b291f674d8
Nikhil Salwade <nikhilsalwade123@gmail.com>
2024-01-29 15:35:50 +0530

    Api for get posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { verifyToken } from './../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create , getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 // Api route for post requests 
 router.post('/create', verifyToken, create);
+
+// Api route for get requests 
+
+router.get('/getposts',  getposts);
 
 export default router;
