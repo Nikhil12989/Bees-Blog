b691edad2ac29e214f3aed1052505516d3f15f6b
Nikhil Salwade <nikhilsalwade123@gmail.com>
2024-01-30 15:23:10 +0530

    get posts api integration and UI for posts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -33,28 +33,24 @@ export const create = async (req, res, next) => {
 
 
 //getposts
-
 export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order || "asc" ? 1 : -1;
-
-    const postsQuery = {
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
       ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
-          { title: { $regex: req.query.searchTerm, $options: "i" } },
-          { content: { $regex: req.query.searchTerm, $options: "i" } },
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
         ],
       }),
-    };
-
-    const posts = await Post.find(postsQuery)
-      .sort({ updatedAt: sortDirection })  // Use 'updatedAt' instead of 'updateAt'
+    })
+      .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
 
@@ -72,7 +68,11 @@ export const getposts = async (req, res, next) => {
       createdAt: { $gte: oneMonthAgo },
     });
 
-    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
   } catch (error) {
     next(error);
   }
