398091652d4f36f3a16c903675d50757e8c3725e
Nikhil Salwade <nikhilsalwade123@gmail.com>
2024-02-03 13:57:24 +0530

    update post Api creation and integration

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from './../utils/verifyUser.js';
-import { create , deletepost, getposts } from '../controllers/post.controller.js';
+import { create , deletepost, getposts, updatepost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
@@ -14,5 +14,9 @@ router.get('/getposts',  getposts);
 //Api route for delete posts 
 
 router.delete('/deletepost/:postId/:userID' , verifyToken, deletepost)
+
+//Api route for update posts 
+
+router.put('/updatepost/:postId/:userID', verifyToken , updatepost)
 
 export default router;
